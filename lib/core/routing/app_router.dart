771b32e57c1dcdab642d0800b0a8707ef09a58f8
Bahnas <bahnas114@gmail.com>
2025-06-23 15:01:26 +0300

    init app

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,24 @@
+import 'package:doctor_app/core/routing/routes.dart';
+import 'package:doctor_app/features/login/login_screen.dart';
+import 'package:doctor_app/features/onbording/onbording_screen.dart';
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  static const String initialRoute = '/';
+
+  static Route<dynamic> onGenerateRoute(RouteSettings settings) {
+    final args = settings.arguments ;
+    switch (settings.name) {
+      case Routes.onbordingScreen:
+        return MaterialPageRoute(builder: (_) => const OnbordingScreen());
+      case Routes.loginScreen:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+      default:
+        return MaterialPageRoute(builder: (_) =>  Scaffold(
+              body: Center(
+                child: Text('No route defined for ${settings.name}'),
+              ),
+            ));
+    }
+  }
+}
---
+++ lib/core/routing/routes.dart
@@ -0,0 +1,4 @@
+class Routes {
+  static const String onbordingScreen = '/onbordingScreen';
+  static const String loginScreen = '/loginScreen';
+}
